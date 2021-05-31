2e957ecba50f3e55733856593a8d0f41c20a748c
Dimitris Trachiotis <acemod13@outlook.com>
2021-05-31 22:08:17 +0300

    Tidying up the new plugin.

--- Battle/FSDK_EscapeCommandCustomizer.js
+++ Battle/FSDK_EscapeCommandCustomizer.js
@@ -90,6 +90,7 @@ var FirehawkADK = FirehawkADK || {};
  * When the player picks the option to escape, this plugin can remove part of
  * the gold they have. If they don't have enough gold to remove, the plugin
  * will remove half of the gold that they have (if a set amount is set).
+ * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  */
 
 var droppedGold = 0;
@@ -138,3 +139,27 @@ this.BattleManager.displayEscapeFailureMessage = function () {
     else
         $gameMessage.add(TextManager.escapeFailure);
 };
+
+/*
+MIT License
+
+Copyright (c) 2021 Studio ACE
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
